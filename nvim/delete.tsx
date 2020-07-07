d4934a99d996f0afb2be203dbe83ca7cd50602c2
William Cory <willcory10@gmail.com>
2020-07-07 09:16:29 -0700

    move everything to .config

---
+++ nvim/delete.tsx
@@ -0,0 +1,16 @@
+class DeleteMe {
+	constructor(){
+		console.log('lol')
+	}
+
+	speak = () => console.log('hellow orld')
+
+	yell = () => console.log('FUUUUUCKKK')
+}
+
+const deleteMe = new DeleteMe()
+
+deleteMe.speak()
+
+deleteMe.yell()
+
